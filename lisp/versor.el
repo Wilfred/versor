9fb6409dce8b83c94a5e0b64a04288c2bc082a24
jcg_sturdy <>
2004-01-26 12:14:10 +0000

    Add GPL

--- lisp/versor.el
+++ lisp/versor.el
@@ -1,5 +1,25 @@
 ;;;; versor.el -- versatile cursor
-;;; Time-stamp: <2003-12-12 12:47:02 john>
+;;; Time-stamp: <2004-01-23 16:43:42 john>
+;;
+;; emacs-versor -- versatile cursors for GNUemacs
+;;
+;; Copyright (C) 2004  John C. G. Sturdy
+;;
+;; This file is part of emacs-versor.
+;; 
+;; emacs-versor is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;; 
+;; emacs-versor is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with emacs-versor; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 (provide 'versor)
 (require 'tempo)
