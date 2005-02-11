501b9264b6fa4316e02fc466b3501e2d8a20f3dd
jcg_sturdy <>
2005-02-11 10:40:17 +0000

    check for buffer / mode changes both before and after commands (in case of changes made in the meantime by a process filter)

--- lisp/buffer-select-hooks.el
+++ lisp/buffer-select-hooks.el
@@ -1,5 +1,5 @@
 ;;;; buffer-select-hooks.el -- do things when noticing that buffer or mode has changed
-;;; Time-stamp: <2004-05-17 12:07:43 john>
+;;; Time-stamp: <2005-02-10 15:49:48 jcgs>
 ;;
 ;; emacs-versor -- versatile cursors for GNUemacs
 ;;
@@ -29,10 +29,27 @@
 (defvar mode-before-command nil
   "The mode which was current before the last command.")
 
+(defun buffer-selection-hook-common ()
+  "Common code for our before and after hooks."
+  ;; modes are less specific than buffers, so do them first
+  (if (not (eq mode-before-command major-mode))
+      (condition-case error-var
+	  ;; (lessage "Running mode selection hook")
+	  (run-hook-with-args 'mode-selection-hook mode-before-command)
+	(error (message "error in mode-selection-hook")
+	       (with-output-to-temp-buffer "*mode selection hook error*"
+		 (backtrace)))))
+  (if (not (eq (current-buffer) buffer-before-command))
+      (condition-case error-var
+	  ;; (lessage "Running buffer selection hook")
+	  (run-hook-with-args 'buffer-selection-hook buffer-before-command)
+	(error (message "error in buffer-selection-hook"))))
+  (setq buffer-before-command (current-buffer)
+	mode-before-command major-mode))
+
 (defun buffer-selection-pre-command-hook ()
   "Function to run before each command, for detecting commands changing the current buffer."
-  (setq buffer-before-command (current-buffer)
-	mode-before-command major-mode)
+  (buffer-selection-hook-common)
   ;; (lessage "In buffer-selection-pre-command-hook buf=%S mode=%S" buffer-before-command mode-before-command)
 
   ;; to get round something that is meant to prevent danger -- should
@@ -44,21 +61,41 @@
 
 (defun buffer-selection-post-command-hook ()
   "Function to run after each command, for detecting commands changing the current buffer."
-  ;; modes are less specific than buffers, so do them first
   ;; (lessage "In buffer-selection-post-command-hook for %S %S-->%S" this-command mode-before-command major-mode)
   (add-hook 'pre-command-hook 'buffer-selection-pre-command-hook)
-  (if (not (eq mode-before-command major-mode))
-      (condition-case error-var
-	  ;; (lessage "Running mode selection hook")
-	  (run-hook-with-args 'mode-selection-hook mode-before-command)
-	(error (message "error in mode-selection-hook")
-	       (with-output-to-temp-buffer "*mode selection hook error*"
-		 (backtrace)))))
-  (if (not (eq (current-buffer) buffer-before-command))
-      (condition-case error-var
-	;; (lessage "Running buffer selection hook")
-	(run-hook-with-args 'buffer-selection-hook buffer-before-command)
-	(error (message "error in buffer-selection-hook")))))
+  (buffer-selection-hook-common)
+  )
+
+;; (defun buffer-selection-pre-command-hook ()
+;;   "Function to run before each command, for detecting commands changing the current buffer."
+;;   (setq buffer-before-command (current-buffer)
+;; 	mode-before-command major-mode)
+;;   ;; (lessage "In buffer-selection-pre-command-hook buf=%S mode=%S" buffer-before-command mode-before-command)
+
+;;   ;; to get round something that is meant to prevent danger -- should
+;;   ;; I be doing this?  seems safe enough in this case -- I haven't had
+;;   ;; any problems with it, and I've been using it all the time for
+;;   ;; months now
+;;   (add-hook 'post-command-hook 'buffer-selection-post-command-hook)
+;;   )
+
+;; (defun buffer-selection-post-command-hook ()
+;;   "Function to run after each command, for detecting commands changing the current buffer."
+;;   ;; modes are less specific than buffers, so do them first
+;;   ;; (lessage "In buffer-selection-post-command-hook for %S %S-->%S" this-command mode-before-command major-mode)
+;;   (add-hook 'pre-command-hook 'buffer-selection-pre-command-hook)
+;;   (if (not (eq mode-before-command major-mode))
+;;       (condition-case error-var
+;; 	  ;; (lessage "Running mode selection hook")
+;; 	  (run-hook-with-args 'mode-selection-hook mode-before-command)
+;; 	(error (message "error in mode-selection-hook")
+;; 	       (with-output-to-temp-buffer "*mode selection hook error*"
+;; 		 (backtrace)))))
+;;   (if (not (eq (current-buffer) buffer-before-command))
+;;       (condition-case error-var
+;; 	;; (lessage "Running buffer selection hook")
+;; 	(run-hook-with-args 'buffer-selection-hook buffer-before-command)
+;; 	(error (message "error in buffer-selection-hook")))))
 
 (add-hook 'pre-command-hook 'buffer-selection-pre-command-hook)
 (add-hook 'post-command-hook 'buffer-selection-post-command-hook)
