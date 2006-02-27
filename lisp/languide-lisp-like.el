bea61889aa1aa82eade08625dac47d7de002f43b
jcg_sturdy <>
2006-02-27 16:56:54 +0000

    improvements to binding handling

--- lisp/languide-lisp-like.el
+++ lisp/languide-lisp-like.el
@@ -1,5 +1,5 @@
 ;;;; languide-lisp-like.el -- Lisp, Elisp, Scheme definitions for language-guided editing
-;;; Time-stamp: <2006-02-20 11:49:51 john>
+;;; Time-stamp: <2006-02-24 15:23:30 jcgs>
 ;;
 ;; Copyright (C) 2004, 2005, 2006  John C. G. Sturdy
 ;;
@@ -65,7 +65,9 @@
   (insert ")"))
 
 (defun find-next-lisp-binding-outwards ()
-  "Move to the next enclosing binding."
+  "Move to the next enclosing binding.
+Returns whether it found one."
+  (interactive)
   (let ((binding-pattern "(let\\*?\\>"))
     (while (and (outward-once)
 		(not (looking-at binding-pattern))))
@@ -97,21 +99,86 @@
 	      (forward-sexp)
 	      (push (list (buffer-substring-no-properties start (point))) variables))
 	    (goto-char next))))
+      (goto-char bod)
+      (down-list)
+      (when (looking-at "defmod[ae]l") (forward-sexp))
+      (forward-sexp 2)
+      (let ((limit (safe-scan-sexps (point) 1)))
+	(down-list)
+	(skip-to-actual-code)
+	(message "Arglist starts at %d" (point))
+	(let ((old (point))
+	      new)
+	  (while (and (setq new (safe-scan-sexps old 1))
+		      (< new limit))
+	    (push (list (buffer-substring-no-properties old new)) variables)
+	    (goto-char new)
+	    (skip-to-actual-code limit)
+	    (setq old (point)))))
       variables)))
 
-(defmodal move-to-enclosing-scope-last-variable-definition (lisp-mode emacs-lisp-mode lisp-interaction-mode) (&optional variables-needed)
+(defmodal move-to-enclosing-scope-last-variable-definition (lisp-mode emacs-lisp-mode lisp-interaction-mode)
+  (&optional variables-needed)
   "Move to the end of the nearest set of variable bindings.
 This is the place at which you would naturally insert a new
 variable, allowing for its initial value referring to any
 variable already declared.
 Optional arguments list names of variables needed in the definition of the new one.
-This lets clever implementations put the definition as far out as possible.")
+This lets clever implementations put the definition as far out as possible."
+  (if (find-next-lisp-binding-outwards)
+      (let ((best-so-far (point)))
+	;; we've found a let, but let's see if we can find one
+	;; further out that still binds all the variables we need
+	(catch 'far-enough
+	  (while (find-next-lisp-binding-outwards)
+	    (let ((end-of-bindings (save-excursion
+				     (down-list)
+				     (forward-sexp)
+				     (point))))
+	      (if (all-variables-in-scope-p end-of-bindings variables-needed)
+		  (setq best-so-far (point))
+		(throw 'far-enough nil)))))
+	(goto-char best-so-far)
+	(when (looking-at "\\((let\\)[^*]")
+	  (save-excursion
+	    (replace-match "\\1*" nil nil nil 1)))
+	(down-list 2)
+	(let ((old (point))
+	      new)
+	  (while (setq new (safe-scan-sexps old 1))
+	    (setq old new))
+	  (goto-char old)))
+    ;; no let forms, so make a new one at the outer level of the defun
+    (down-list)
+    (forward-sexp 3)			; over "defun", name, args
+    (skip-to-actual-code)
+    (when (= (char-after) ?\")
+      (forward-sexp 1)			; over docstring
+      (skip-to-actual-code))
+    (when (looking-at "(interactive")
+      (skip-to-actual-code))
+    (insert " (let (")
+    (save-excursion	; so we finish at new variable insertion point
+      (insert ")\n")
+      (let ((old (point))
+	    new)
+	(while (setq new (safe-scan-sexps old 1)) ; move over body forms
+	  (setq old new))
+	(goto-char old))
+      (insert ")")
+      (backward-up-list)
+      (indent-sexp))))
 
 (defmodal insert-variable-declaration (lisp-mode emacs-lisp-mode lisp-interaction-mode) (name type initial-value)
   "Insert a definition for a variable called NAME, of TYPE, with INITIAL-VALUE.
 Assumes we are at the obvious point to add a new variable.
 TYPE and INITIAL-VALUE may be null, but the NAME is required."
-  (insert "(" name " " initial-value ")"))
+  (when (= (char-before) close-bracket) 
+    (insert "\n"))
+  (lisp-indent-line)
+  (save-excursion
+    (insert "(" name " " initial-value ")"))
+  (indent-sexp))
 
 (defun arg-name (arg)
   "Return just the name of ARG, which is in languide's format, which may be just the name, or (name . type)."
@@ -193,13 +260,20 @@ Each element is a list of:
 	  (goto-char keystart)
 	  (cond
 	   ((looking-at "lambda")
-	    (let* ((name-start (safe-scan-lists keyend 1 -1))
+	    (let* ((names-start (safe-scan-lists keyend 1 -1))
+		   (names-end (1- (safe-scan-sexps (1- names-start) 1)))
+		   (name-start names-start)
 		   (name-end (safe-scan-sexps name-start 1)))
-	      (push (list (buffer-substring-no-properties name-start name-end)
-			  nil
-			  keyend scope-end
-			  nil)
-		    result)))
+	      (while (<= name-end names-end)
+		(push (list (buffer-substring-no-properties name-start name-end)
+			    nil
+			    keyend scope-end
+			    nil)
+		      result)
+		(goto-char name-end)
+		(skip-to-actual-code)
+		(setq name-start (point)
+		      name-end (safe-scan-sexps name-start 1)))))
 	   ((looking-at "let\\*?\\>")
 	    (let ((star (looking-at "let\\*")))
 	      (goto-char keyend)
@@ -312,7 +386,8 @@ Each element is a list of:
   (body "(while" (expression) (expressions))
   (create (template & > "(while " p n>
 		    r ")"))
-  (begin-end "(while " ")"))
+  (begin-end "(while " ")")
+  (begin-end-with-dummy "(while true " ")"))
 
 (defstatement unless (lisp-mode emacs-lisp-mode lisp-interaction-mode)
   "Emacs-lisp \"unless\" special form."
@@ -322,7 +397,8 @@ Each element is a list of:
   (create (precondition (require 'cl))
 	  (template & > "(unless " p n>
 		    r ")"))
-  (begin-end "(unless " ")"))
+  (begin-end "(unless " ")")
+  (begin-end-with-dummy "(unless false " ")"))
 
 (defstatement condition-chain (lisp-mode emacs-lisp-mode lisp-interaction-mode)
   ""
@@ -363,7 +439,8 @@ Each element is a list of:
   (create (precondition (require 'cl))
 	  (template & > "(when " p n>
 		    r ")"))
-  (begin-end "(when \n" ")"))
+  (begin-end "(when \n" ")")
+  (begin-end-with-dummy "(when true \n" ")"))
 
 (defstatement if-then-else (lisp-mode emacs-lisp-mode lisp-interaction-mode scheme-mode)
   "If statement with else clause."
@@ -374,17 +451,20 @@ Each element is a list of:
   (create (template & > "(if " p n>
 		    r n>
 		    p ")"))
-  (begin-end "(if" ")"))
+  (begin-end "(if" ")")
+  (begin-end-with-dummy "(if true " ")"))
 
 (defstatement and (lisp-mode emacs-lisp-mode lisp-interaction-mode scheme-mode)
   "And expression."
   (keyword "and")
-  (begin-end "(and " ")"))
+  (begin-end "(and " ")")
+  (begin-end-with-dummy "(and true " ")"))
 
 (defstatement or (lisp-mode emacs-lisp-mode lisp-interaction-mode scheme-mode)
   "Or expression."
   (keyword "or")
-  (begin-end "(or " ")"))
+  (begin-end "(or " ")")
+  (begin-end-with-dummy "(or false " ")"))
 
 (defstatement not (lisp-mode emacs-lisp-mode lisp-interaction-mode scheme-mode)
   "Not expression."
