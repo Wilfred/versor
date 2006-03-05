d629738116ce9a62a049a8c9447047113064d3a2
jcg_sturdy <>
2006-03-05 10:24:09 +0000

    add global-making command

--- lisp/languide-keymap.el
+++ lisp/languide-keymap.el
@@ -1,5 +1,5 @@
 ;;;; languide-keymap.el -- keymap and menu setup for languide
-;;; Time-stamp: <2006-02-10 11:32:15 jcgs>
+;;; Time-stamp: <2006-02-28 10:55:06 jcgs>
 
 ;;  This program is free software; you can redistribute it and/or modify it
 ;;  under the terms of the GNU General Public License as published by the
@@ -20,10 +20,12 @@
 
 (defvar versor:languide-menu-items
   '(["Use local variable for selection" versor:convert-selection-to-variable t]
+    ["Use global variable for selection" versor:convert-selection-to-global-variable t]
     ["Convert selection to function" versor:convert-selection-to-function t]
     ["Surround selection with function call" versor:surround-selection-with-call t]
     ["Remove function call" versor:remove-function-call t]
     ["Unify statements" versor:unify-statements t]
+    ["Comment selection" versor:comment-selection t]
     ["Move to enclosing scoping point" versor:enclosing-scoping-point t]
     ["Move to enclosing decision point" versor:enclosing-decision-point t]
     ["Employ variable" versor:employ-variable t]
@@ -44,10 +46,12 @@
 
 (define-key languide-map "v" 'versor:convert-selection-to-variable)
 (define-key languide-map "f" 'versor:convert-selection-to-function)
+(define-key languide-map "g" 'versor:convert-selection-to-global-variable)
 (define-key languide-map "(" 'versor:surround-selection-with-call)
 (define-key languide-map ")" 'versor:remove-function-call)
 (define-key languide-map "{" 'versor:unify-statements)
 (define-key languide-map "=" 'versor:enclosing-scoping-point)
 (define-key languide-map "?" 'versor:enclosing-decision-point)
+(define-key languide-map ";" 'versor:comment-selection)
 
 ;;; end of languide-keymap.el
