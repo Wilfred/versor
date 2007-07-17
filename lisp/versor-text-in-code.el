c687dbea8e5977e66645347ff55bbb83ef51010b
jcg_sturdy <>
2007-07-17 12:50:16 +0000

    new files

--- lisp/versor-text-in-code.el
+++ lisp/versor-text-in-code.el
@@ -1,26 +1,25 @@
 ;;; versor-text-in-code.el -- versatile cursor handling of strings and comments
-;;; Time-stamp: <2006-08-02 12:18:07 john>
+;;; Time-stamp: <2007-06-25 14:32:14 jcgs>
 ;;
 ;; emacs-versor -- versatile cursors for GNUemacs
 ;;
 ;; Copyright (C) 2004, 2006  John C. G. Sturdy
 ;;
 ;; This file is part of emacs-versor.
-;; 
+;;
 ;; emacs-versor is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2 of the License, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; emacs-versor is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with emacs-versor; if not, write to the Free Software
 ;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
 
 (require 'versor-names)
 (require 'versor-dimensions)
@@ -80,7 +79,7 @@ Meant to go on post-command-hook."
 		      versor-meta-level versor-non-text-meta-level
 		      versor-level versor-non-text-level)
 		(versor-set-status-display t)
-		(when versor-announce-text-in-code 
+		(when versor-announce-text-in-code
 		  (message "Left comment or string, returning to %s:%s"
 			   (versor-meta-level-name versor-meta-level)
 			   (versor-level-name versor-level)))))))
--- lisp/versor-voice.el
+++ lisp/versor-voice.el
@@ -1,22 +1,22 @@
 ;;;; versor-voice.el
-;;; Time-stamp: <2006-08-03 20:23:39 john>
+;;; Time-stamp: <2007-06-20 19:57:01 jcgs>
 ;;
 ;; emacs-versor -- versatile cursors for GNUemacs
 ;;
 ;; Copyright (C) 2004, 2005, 2006  John C. G. Sturdy
 ;;
 ;; This file is part of emacs-versor.
-;; 
+;;
 ;; emacs-versor is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2 of the License, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; emacs-versor is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with emacs-versor; if not, write to the Free Software
 ;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
