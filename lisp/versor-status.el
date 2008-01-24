9365bd51dafd7a79d66516486f19a83b9187ea5e
jcg_sturdy <>
2008-01-24 10:53:11 +0000

    comment and tidy

--- lisp/versor-status.el
+++ lisp/versor-status.el
@@ -1,9 +1,9 @@
 ;;; versor-status.el -- versatile cursor
-;;; Time-stamp: <2007-07-16 14:07:21 jcgs>
+;;; Time-stamp: <2007-08-27 17:18:38 jcgs>
 ;;
 ;; emacs-versor -- versatile cursors for GNUemacs
 ;;
-;; Copyright (C) 2004, 2005, 2006  John C. G. Sturdy
+;; Copyright (C) 2004, 2005, 2006, 2007  John C. G. Sturdy
 ;;
 ;; This file is part of emacs-versor.
 ;; 
@@ -41,7 +41,16 @@ The result is in the form of a property list."
     result))
 
 (defun versor-set-status-display (&optional one of-these explicit no-message)
-  "Indicate the state of the versor system."
+  "Indicate the state of the versor system.
+Optional args: you can mark ONE OF-THESE; if OF-THESE is not given, just
+show ONE.
+
+EXPLICIT means pop up a multi-line display even if this is not normally done.
+
+NO-MESSAGE means work quietly, for example when we are changing
+the cursor and selection colour because of having switched
+buffer."
+  ;; get the level names, and colour them in if told to
   (setq versor-current-over-level-name
 	(if versor-color-dimension-indicators
 	    (propertize (versor-level-name
@@ -62,21 +71,29 @@ The result is in the form of a property list."
 	  (versor-level-name versor-level))
 	versor-current-meta-level-name
 	(versor-meta-level-name versor-meta-level))
+  (when (and (eq major-mode 'emacs-lisp-mode)
+	     (eq versor-level 1)
+	     (eq versor-meta-level 1))
+    (message "Warning! wrong dimensions for emacs-lisp-mode: %d:%d" versor-level versor-meta-level)
+    (backtrace))
+  ;; pop up a message (possibly multi-line)
   (if (and versor-multi-line-level-display explicit)
       (versor-display-current-dimensions)
     (unless no-message
-	(if one
-	    (if of-these
-		(versor-display-highlighted-choice one of-these)
-	      (if (stringp one)
-		  (message one)))
-	  (message (first (versor-current-level))))))
+      (if one
+	  (if of-these
+	      (versor-display-highlighted-choice one of-these)
+	    (if (stringp one)
+		(message one)))
+	(message (first (versor-current-level))))))
+  ;; set up the mode line indicators
   (if versor-reversible
       (setq versor-mode-line-begin-string (if versor-reversed " <==" " <")
 	    versor-mode-line-end-string (if versor-reversed ">" "==>"))
     (setq versor-mode-line-begin-string " <"
 	  versor-mode-line-end-string ">"))
   (force-mode-line-update t)
+  ;; set the cursor colour
   (when versor-change-cursor-color
     ;; done: allow for a second choice of colour, to be used if the main one is the same as the background colour
     (let ((color (versor-action (versor-current-level) 'color)))
@@ -84,6 +101,9 @@ The result is in the form of a property list."
 		 (versor-action (versor-current-level) 'other-color))
 	(setq color (versor-action (versor-current-level) 'other-color)))
       (set-cursor-color color)))
+  ;; set the value for the face attribute to be used to indicate the
+  ;; selection; we normally use background-color, but others can be
+  ;; specified by the user
   (when (and window-system
 	     versor-item-attribute
 	     (fboundp 'set-face-attribute))
@@ -99,6 +119,7 @@ The result is in the form of a property list."
       (apply 'set-face-attribute 'versor-item-face nil
 	     (versor-gather-level-attributes (versor-current-level)
 					     versor-item-attribute)))))
+  ;; confirmation by voice for those who want it
   (versor-speak "Moving by %s %s%s%s"
 		versor-current-meta-level-name
 		versor-current-level-name
